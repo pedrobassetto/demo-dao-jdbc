25c1b4f8659017fade2f89185f9c344cb2a6cbf2
Pedro Bassetto <contato@pedrobassetto.com>
2020-05-01 20:06:18 -0300

    Reusando Instanciações

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,8 +13,8 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== Teste 1: seller findById ===");
         Seller seller = sellerDao.findById(2);
-
         System.out.println(seller);
     }
 }
