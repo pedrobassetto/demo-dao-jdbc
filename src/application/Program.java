e534d0839d4bffee598be298e6c86f4c25b1bd3e
Pedro Bassetto <contato@pedrobassetto.com>
2020-05-01 22:29:20 -0300

    Implementando insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,5 +30,11 @@ public class Program {
         for(Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n=== Teste 4: seller Insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 40000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserido: Novo Id: "+ newSeller.getId());
+
     }
 }
