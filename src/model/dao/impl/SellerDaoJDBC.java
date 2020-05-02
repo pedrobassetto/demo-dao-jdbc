ae3a4b116ce923a75a7a17e1044865e6c247eb4d
Pedro Bassetto <contato@pedrobassetto.com>
2020-05-01 21:59:31 -0300

    Implementando findAll

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -68,7 +68,37 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*, department.Name as DepName "
+                            + "FROM seller "
+                            + "INNER JOIN department ON seller.DepartmentId = department.id "
+                            + "ORDER BY Name "
+            );
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while(rs.next()){
+                Department department = map.get(rs.getInt("DepartmentId"));
+                if(department == null){
+                    department = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), department);
+                }
+
+                Seller obj = instantiateSeller(rs, department);
+                list.add(obj);
+            }
+            return list;
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
